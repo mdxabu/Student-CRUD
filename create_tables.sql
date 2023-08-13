a671235314e0ed3e60c01903c7cbaa9399f83c55
Mohamed Abdullah <abdullahfakrudeen2020@gmail.com>
2023-08-13 19:08:51 +0530

    ✅Maked some Columns are UNIQUE

--- create_tables.sql
+++ create_tables.sql
@@ -5,28 +5,32 @@ CREATE TABLE CSE(
 name VARCHAR(20),
 email VARCHAR(50),
 phoneNo VARCHAR(10),
-dept VARCHAR(20)
+dept VARCHAR(20),
+UNIQUE(email,phoneNo)
 );
 
 CREATE TABLE IT(
     name VARCHAR(20),
     email VARCHAR(50),
     phoneNo VARCHAR(10),
-    dept VARCHAR(20)
+    dept VARCHAR(20),
+    UNIQUE(email,phoneNo)
 );
 
 CREATE TABLE EEE(
     name VARCHAR(20),
     email VARCHAR(50),
     phoneNo VARCHAR(10),
-    dept VARCHAR(20)
+    dept VARCHAR(20),
+    UNIQUE(email,phoneNo)
 );
 
 CREATE TABLE ECE(
     name VARCHAR(20),
     email VARCHAR(50),
     phoneNo VARCHAR(10),
-    dept VARCHAR(20)
+    dept VARCHAR(20),
+    UNIQUE(email,phoneNo)
 );
 
 CREATE TABLE history(
